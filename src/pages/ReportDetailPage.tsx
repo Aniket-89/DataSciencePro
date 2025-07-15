55ab10f13ada7834405dd23f2d0ae54de8d09e01
Aniket Kumar <jarvis@aniketkumarg99@gmail.com>
2025-07-16 02:23:11 +0530

    type fix

--- src/pages/ReportDetailPage.tsx
+++ src/pages/ReportDetailPage.tsx
@@ -2,7 +2,6 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { db } from "../firebaseConfig";
 import { doc, getDoc } from "firebase/firestore";
-import TopBanner from "../components/TopBanner";
 import { Placeholder } from "../assets/assets";
 
 type Report = {
